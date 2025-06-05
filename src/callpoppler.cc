80e65db163cfea39cdaabc1671e0a0ed1c08f862
Michael Tarasov <mikhail@tarasovmn.de>
2025-06-05 09:51:57 +0200

    Update callpoppler.cc

--- src/callpoppler.cc
+++ src/callpoppler.cc
@@ -94,7 +94,7 @@ extern "C" ResultCode pdftotext_print_with_layout(char *filename, void * stream,
 
         doc->displayPage(&textOut, pageNum, 72.0, 72.0, 0, true, false, false);
 
-        TextPage *page = textOut->takeText();
+        TextPage *page = textOut.takeText();
         page->dump(stream, output_f, true, eolUnix, false);
         page->decRefCnt();
     }
